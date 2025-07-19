0d4e2edc6fbd99c37ab597494f2eaaa0dadd4b6a
loganpanwar <vishvendar@gmail.com>
2025-07-20 00:35:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "🔥 Hello this is vishvendar from my second Flask Docker app!"
+    return "🔥 Hello this is vishvendar bhai from my second Flask Docker app!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
